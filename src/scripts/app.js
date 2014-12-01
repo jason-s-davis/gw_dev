10b8e69aa190b12416240754c7c43c57128ea6b5
Jason Davis <jdavis37@ggc.edu>
2014-12-01 11:56:53 -0500

    [update] added firefox fix and close behavior

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,6 +5,101 @@ require('jquery-mousewheel')($);
 var jQuery = $;
 require('../../libs/scrollbar/jquery.mCustomScrollbar');
 require('./infscr');
+require('./ads');
+
+(function($){
+
+/// Sub Nav Area Functionality
+var closer;
+
+function SubNav(event){
+
+		window.clearTimeout(closer);
+		
+		var target = $(event.target).attr('targ')
+
+		// remove red from item and link
+		$('#MainNav ul li').removeClass('selected');
+		$('#MainNav ul li a').removeClass('selected');
+		
+		// add red background to hovered item
+		$(event.target).addClass('selected');
+		// hide all of the sub nav
+		$('#SideSubNav ul').hide();
+		// show the subnav for the hovered item
+		$('#' + target + '-nav').css('display', 'block');
+
+		// slide the main content over to the right
+		$('#MainContent').css('margin-left', '320px');
+
+}
+
+// activate SubNav on mouseover or click of main nav
+$("#MainNav").on("click mouseover", "ul li", SubNav);
+
+$('main').on('mouseenter', function (event) {
+	closer = window.setTimeout(function () {
+			$('#MainContent').css('margin-left', '175px');
+			console.log('here')
+		}, 500);
+});
+
+function SideBar(){
+
+	if($("#sidebar-ad-placeholder").height() < 250){
+		ad_offset = 250
+	} else {
+		ad_offset = $("#sidebar-ad-placeholder").height();
+	}
+	var offset =  $(window).height() - ($('#HeaderContent').height() + ad_offset + 70);
+	$("#SideBar").css({'margin-top': ($(window).scrollTop()), 'height': offset});
+	$("#sidebar-ad-placeholder").css({'position': 'fixed', 'bottom': 10});
+}
+
+function HeaderHeight() {
+	$('.header-push').css('padding-top', $('#HeaderContent').height() + 10);
+}
+
+
+// $(document).ready( function(){
+
+	HeaderHeight();
+	SideBar();
+
+	$(window).resize(function() {
+		SideBar();
+		HeaderHeight();
+	})
+
+	/**  Keep the sidebar up top **/
+	$(window).scroll(function(){
+	$("#SideBar")
+		.stop()
+		SideBar();
+	});
+
+// });
+
+
+/*******************************************************/
+
+// Move Content Area left and right on click on menu button
+$( ".shownav" ).click(function() {
+
+	// If nav closed, open it.
+	if ($('#MainContent').css('margin-left') == '30px') {
+		$('#MainContent').css('margin-left','175px');
+	} else {
+		$('#MainContent').css('margin-left','30px');
+	}
+});
+
+
+
+
+
+/*******************************************************/
+
 
 
 // ********* Init custom scrollbars here  ********* //
@@ -23,82 +118,10 @@ $(".TabPanel").mCustomScrollbar({
     theme:"minimal-dark"
 });
 
-/// Sub Nav Area Functionality
-
-function SubNav(){
-
-	var target = $(event.target).attr('targ')
-	// console.log(event.target);
-	$('#MainNav ul li').removeClass('selected');
-	$('#MainNav ul li a').removeClass('selected');
-	$(event.target).addClass('selected');
-	$('#SideSubNav ul').hide();
-	
-	$('#' + target + '-nav').css('display', 'block');
-	$('#MainContent').css('margin-left', '320px');
-}
-
-$( "#MainNav ul li" ).mouseover(function() {
-  SubNav();
-});
-
-$( "#MainNav ul li" ).click(function() {
-  SubNav();
-});
-
-
-function SideBar(){
-	if($("#sidebar-ad-placeholder").height() < 250){
-		ad_offset = 250
-	} else {
-		ad_offset = $("#sidebar-ad-placeholder").height();
-	}
-	var offset =  $(window).height() - ($('#HeaderContent').height() + ad_offset + 70);
-	$("#SideBar").css({'margin-top': ($(window).scrollTop()), 'height': offset});
-	$("#sidebar-ad-placeholder").css({'position': 'fixed', 'bottom': 10});
-}
-
-function HeaderHeight() {
-	$('.header-push').css('padding-top', $('#HeaderContent').height() + 10);
-}
-
-
-$(document).ready( function(){
-	HeaderHeight();
-	SideBar();
-
-	$(window).resize(function() {
-		SideBar();
-		HeaderHeight();
-	})
-
-	/**  Keep the sidebar up top **/
-	$(window).scroll(function(){
-	$("#SideBar")
-		.stop()
-		SideBar();
-});
-});
-
-
-/*******************************************************/
-
-// Move Content Area left and right on click on menu button
-$( ".shownav" ).click(function() {
-	// If nav closed, open it.
-	if ($('#MainContent').css('margin-left') == '30px') {
-		$('#MainContent').css('margin-left','175px');
-	} else {
-		$('#MainContent').css('margin-left','30px');
-	}
-});
-
-
-
-/*******************************************************/
-
-///  Tabs Functionality Init.
+// *********** Tabs Functionality Init.  ************ //
 $('#tabs a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 })
+
+})(jQuery);
