33c96a354606273f06d4c35969925a8841c92c21
Jason Davis <jdavis37@ggc.edu>
2014-12-11 14:03:35 -0500

    [update] updated js build script

--- gulp/config.js
+++ gulp/config.js
@@ -55,11 +55,6 @@ module.exports = {
           entries: './src/scripts/app.js',
           dest: dest + '/scripts/',
           outputName: 'app.min.js'
-        },
-        {
-          entries: './src/scripts/infscr.js',
-          dest: dest + '/scripts/',
-          outputName: 'infscr.min.js'
         }
     ]
   }
